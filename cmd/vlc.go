4283d0276d4cf816fb71ca246cd3adf806219b4f
Atlas <btw210899@gmail.com>
2023-08-15 18:21:52 +0300

    project init

---
+++ cmd/vlc.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var vlcCmd = &cobra.Command{
+	Use:   "vlc",
+	Short: "Pack file using variable-length code",
+	Run:   pack,
+}
+
+func init() {
+	packCmd.AddCommand(vlcCmd)
+}
+
+const packedExtension = "vlc"
+
+var ErrEmptyPath = errors.New("path to file is not specified")
+
+func pack(_ *cobra.Command, args []string) {
+	if len(args) < 1 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+
+	filePath := args[0]
+	r, err := os.Open(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		handleErr(err)
+	}
+
+	packed := "" + string(data) // TODO: replace by Encode(data)
+
+	err = os.WriteFile(packedFileName(filePath), []byte(packed), 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+func packedFileName(path string) string {
+	fileName := filepath.Base(path)
+	baseName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+
+	return baseName + "." + packedExtension
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gorchiver/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
